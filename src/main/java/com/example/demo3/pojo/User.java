40d15b946532719ddab4ccd3fc78ea23608b4d01
ladyyyy <897858087@qq.com>
2020-09-11 09:25:57 +0800

    commit messege 9-11 user pojo

--- src/main/java/com/example/demo3/pojo/User.java
+++ src/main/java/com/example/demo3/pojo/User.java
@@ -1,6 +1,40 @@
 package com.example.demo3.pojo;
 
 public class User {
-    private   String username ;
-    private  String password; 
+    private String username ;
+    private  String userid ;
+    private  String password ;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", userid='" + userid + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
